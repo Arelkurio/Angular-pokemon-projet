1e6e1b07cc3cf8681489df89f31ad989405964e1
Thibault-Demasi <82362218+Thibault-Demasi@users.noreply.github.com>
2021-12-28 14:07:04 +0100

    Add files via upload

---
+++ src/pipe/abilities-filter.pipe.ts
@@ -0,0 +1,32 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'abilitiesFilter'
+})
+export class AbilitiesFilterPipe implements PipeTransform {
+
+  transform(value: any, filters?: any): any {
+    if (!value) {
+      return;
+    }
+    if (!filters) {
+      return value;
+    }
+
+    return value.filter((item: any) => {
+      let matchFound = true;
+
+      if (item.details && item.details.abilities) {
+        filters.forEach((filter: any) => {
+          if (matchFound) {
+            matchFound = JSON.stringify(item.details.abilities)
+            .toLowerCase()
+            .includes(filter);
+            }
+          });
+      }
+      return matchFound;
+    });
+  }
+
+}
