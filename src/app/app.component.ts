24e71177ce05c250a0d5a4f2be7b9a8528ffb0c1
Thibault-Demasi <82362218+Thibault-Demasi@users.noreply.github.com>
2021-12-28 14:06:37 +0100

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { Results } from './interface'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public search!: string;
+  public typeFilter!: string;
+  public pokemons!: Array<Results>;
+  public abilityFilter!: Array<string>
+
+  newPokemonSearch(search: string): void {
+    if (this.search !== search) {
+      this.search = search;
+    }
+  }
+
+  newTypeSelected(filter: string): void {
+    if (this.typeFilter !== filter) {
+      this.typeFilter = filter;
+    }
+  }
+
+  newAbilitiesSelected(abilities: Array<string>): void {
+    if (this.abilityFilter !== abilities) {
+      this.abilityFilter = abilities;
+    }
+  }
+
+  exportPokemons(pokemons: Array<Results>): void {
+    if (this.pokemons !== pokemons) {
+      this.pokemons = pokemons;
+    }
+  }
+}
