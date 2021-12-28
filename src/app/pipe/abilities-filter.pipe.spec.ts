392b3a3aa8321fdad479b73e121ef26fcac9bb53
Thibault-Demasi <82362218+Thibault-Demasi@users.noreply.github.com>
2021-12-28 13:43:00 +0100

    Delete src directory

--- src/app/pipe/abilities-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AbilitiesFilterPipe } from './abilities-filter.pipe';
-
-describe('AbilitiesFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new AbilitiesFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
